5332b106ee16f78d0dd4330a76d208d8024138e4
smash95 <31133579+smash95@users.noreply.github.com>
2017-08-22 14:02:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,6 @@
-console.log('Loaded!');
+var button=document.getElementById('counter');
+button.onClick=function(){
+    counter=counter+1;
+    var span=docuent.getElementById('count');
+    span.innerHTML=countr.toString();
+};
